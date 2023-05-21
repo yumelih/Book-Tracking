d160cdb71b6f9a1cd6cf26a0f2b03306ff41c313
yumelih <yumelih@gmail.com>
2023-05-21 12:12:38 +0300

    Delete all the books option added

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 //# 1) Add a Book Logic
 
 const bookShell = document.querySelector(".book-shell_container");
-console.log(bookShell);
 const addBookContainer = document.querySelector(".add-book_container");
 
 const modal = document.getElementById("my-modal");
@@ -13,7 +12,8 @@ const modalForm = document.querySelector(".modal-form");
 const bookName = modalForm["book-name"];
 const bookAuthor = modalForm["book-author"];
 const bookImg = modalForm["book-img"];
-console.log(bookImg);
+
+const deleteAllBtn = document.querySelector(".nav__delete-btn");
 
 let oldBooks = JSON.parse(localStorage.getItem("books") || "[]");
 
@@ -24,7 +24,7 @@ function saveToLocalStorage(book) {
   if (oldBooks) {
     newBooks = [...oldBooks, book];
   }
-  console.log(newBooks);
+
   if (oldBooks) {
     localStorage.setItem("books", JSON.stringify(newBooks));
   } else {
@@ -46,6 +46,7 @@ modalForm.addEventListener("submit", function (event) {
   location.reload();
 });
 
+//* Render the page with books from localStorage
 window.addEventListener("load", function () {
   // Retrieve the relevant localStorage data
   let data = JSON.parse(localStorage.getItem("books"));
@@ -53,7 +54,6 @@ window.addEventListener("load", function () {
   // Check if the data exists
   if (data !== null) {
     // Render the data on the page
-    console.log(data);
     data.forEach((book) => {
       let html = `<div class="book_container">
         <img src=${book.bookImg} alt="book cover" />
@@ -62,6 +62,16 @@ window.addEventListener("load", function () {
       </div>`;
       bookShell.insertAdjacentHTML("afterbegin", html);
     });
+  }
+});
+
+//* Delete all the books
+deleteAllBtn.addEventListener("click", function (event) {
+  if (event.target === deleteAllBtn) {
+    if (confirm("Are you sure, this will delete all the books") === true) {
+      localStorage.setItem("books", JSON.stringify([]));
+      location.reload();
+    }
   }
 });
 
